0a730325183e83ca2c0704a095d12cd2120f037a
Fraser Jubb <fraser.jubb@gmail.com>
2022-11-26 21:45:00 +0000

    Made progress with homework. Answered 10/23.

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,51 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
+
+def get_total_cash(total_cash):
+    return total_cash["admin"]["total_cash"]
+   
     
-    # def test_pet_shop_name(self):
-    #     name = get_pet_shop_name(self.cc_pet_shop)
-    #     self.assertEqual("Camelot of Pets", name)
+def add_or_remove_cash(pet_shop, cash):
+    #take the original cash total and adds cash
+    pet_shop["admin"]["total_cash"] += cash
+    return pet_shop["admin"]["total_cash"]
+
+
+def get_pets_sold(pets_sold):
+    return pets_sold["admin"]["pets_sold"]
+
+
+def increase_pets_sold(pet_shop, new_pets_sold):
+    pet_shop["admin"]["pets_sold"] += new_pets_sold
+    return new_pets_sold
+
+
+# def get_stock_count(count):
+#     count = ["pets"].index(1)
+#     return count
+    # for pet_number in pet_shop:
+    #     if len(["pets"]) == count:
+    #         return pet_number
+    # stock_count += pet_number
+    # return stock_count
+
+
+def get_pets_by_breed(pet_shop, breed_given):
+    named_breed = []
+    for breed in pet_shop["pets"]:
+        if breed["breed"] == breed_given:
+            named_breed.append(breed)
+    return named_breed
+
+
+def find_pet_by_name(pet_shop, name):
+    for name_of_pet in pet_shop["pets"]:
+        if name_of_pet["name"] == name:
+            return name_of_pet
+    
+
+
 
 
