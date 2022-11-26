873c6e1fa2537cc043a64832d47558cae39c9ffe
Fraser Jubb <fraser.jubb@gmail.com>
2022-11-26 10:36:02 +0000

    set-up github repository and uploaded initial files

---
+++ src/pet_shop.py
@@ -0,0 +1,9 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+    
+    # def test_pet_shop_name(self):
+    #     name = get_pet_shop_name(self.cc_pet_shop)
+    #     self.assertEqual("Camelot of Pets", name)
+
+
